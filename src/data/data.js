0dfa26b84223ad1ce72bc9d5f2148f28adda311c
Annabel Tu <annabeltu9@gmail.com>
2024-08-16 12:45:22 -0400

    movie picker

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        img: "https://www.tvguide.com/a/img/catalog/provider/1/2/1-10296726314.jpg",
+        name: "Legally Blonde",
+        genre: "Comedy/Romance"
+    },
+    {
+        img: "https://musicart.xboxlive.com/7/1fb26500-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+        name: "How to Lose a Guy in 10 Days",
+        genre: "Comedy/Romance"
+    },
+    {
+        img: "https://m.media-amazon.com/images/M/MV5BMTA5MTE2Mzc5MjReQTJeQWpwZ15BbWU4MDk0Njg2ODUx._V1_FMjpg_UX1000_.jpg",
+        name: "Descendants",
+        genre: "Fantasy/Musical"
+    }
+
+];
